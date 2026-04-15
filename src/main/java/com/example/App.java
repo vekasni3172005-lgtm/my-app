7495ccc6f9c12b28361a7aac048b9339e2c300d4
vekasni3172005-lgtm <vekasni3172005@gmail.com>
2026-04-16 01:36:20 +0530

    Update main method to reflect CI/CD message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,7 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("CI/CD Pipeline using Maven and Jenkins is working!");
     }
 }
